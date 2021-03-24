275d3eb60a3262741700a32004e9beaa26c3b09a
CoffeeLover <46829936+BrunoGLEM@users.noreply.github.com>
2021-03-24 02:15:50 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+import time
+import random
+
+count = 1
+def Farm():
+    global count
+    print("Script Count: "+str(count))
+    data = {"content":"pls pm"}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":"f"}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":"pls fish"}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+    
+    data = {"content":"pls hunt"}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":"pls beg",}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":"pls highlow"}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":highlow[random.randrange(0,1)]}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":"pls use padlock"}
+    r.post(url, headers=headers, data=data)
+    time.sleep(1)
+
+    data = {"content":"pls dep max"}
+    r.post(url, headers=headers, data=data)
+    count += 1
+    time.sleep(40)
+
+load_dotenv()
+Authorization = os.getenv('Authorization_Token')
+url = os.getenv('url')
+headers = {'Authorization': Authorization,}
+highlow = ["low","high"]
+r = requests.session()
+while(True):
+    Farm()
